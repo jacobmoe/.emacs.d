961377b9a7da5da7c9bffc82b5d859b7ce21b343
Jacob Moeller <jacobmoe@gmail.com>
2015-01-31 16:08:34 -0500

    some smartparens wrap commands

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -3,4 +3,9 @@
 
 (smartparens-global-mode 1)
 
+;; :wrap defines keybindings for the wrap
+;; useful with evil-mode
+(sp-pair "(" ")" :wrap "C-)")
+(sp-pair "\"" "\"" :wrap "C-\"")
+
 (provide 'init-smartparens)
---
+++ packages/README.md
@@ -0,0 +1,1 @@
+Packages that aren't installed with the package manager are kept here. A list of all packages found in `lisp/set-packages.el`.
